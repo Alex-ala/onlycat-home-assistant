e458ee43cda2ece1d6a7e101ee753b9232ceeec9
Alexander Landmesser <alexander@landmesser.online>
2026-02-19 21:29:35 +0100

    add log

--- custom_components/onlycat/binary_sensor_device_errors.py
+++ custom_components/onlycat/binary_sensor_device_errors.py
@@ -98,4 +98,5 @@ class OnlyCatErrorSensor(CoordinatorEntity, BinarySensorEntity):
             is not None
             else {}
         )
+        _LOGGER.debug("Updating OnlyCatErrorSensor for device %s: %s",self.device.device_id, self._attr_extra_state_attributes)
         self.async_write_ha_state()
